cfdef1b1b6fb2a2256f14eb02153f282ae7397fe
Volodymyr Novikov <soulesrar@gmail.com>
2026-05-23 11:28:47 +0200

    Initial Commit

---
+++ Nunc/NuncApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuncApp.swift
+//  Nunc
+//
+//  Created by Wlodzimierz on 23/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct NuncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
